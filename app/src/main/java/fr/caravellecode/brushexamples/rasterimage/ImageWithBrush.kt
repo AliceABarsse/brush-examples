aad175714e1cde080ebdd3b96d1edeef0f6249a4
Alice Barsse <alice.barsse@gmail.com>
2024-03-13 18:14:09 +0100

    Combine Pattern and color BlendModes to see all results possible
    Do exclude values that really do not suit our purpose: no image, no pattern, or no color

--- app/src/main/java/fr/caravellecode/brushexamples/rasterimage/ImageWithBrush.kt
+++ app/src/main/java/fr/caravellecode/brushexamples/rasterimage/ImageWithBrush.kt
@@ -18,22 +18,28 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.BlendMode.Companion.Clear
 import androidx.compose.ui.graphics.BlendMode.Companion.Difference
 import androidx.compose.ui.graphics.BlendMode.Companion.Dst
+import androidx.compose.ui.graphics.BlendMode.Companion.DstAtop
 import androidx.compose.ui.graphics.BlendMode.Companion.DstIn
 import androidx.compose.ui.graphics.BlendMode.Companion.DstOut
+import androidx.compose.ui.graphics.BlendMode.Companion.DstOver
 import androidx.compose.ui.graphics.BlendMode.Companion.Exclusion
 import androidx.compose.ui.graphics.BlendMode.Companion.Hardlight
+import androidx.compose.ui.graphics.BlendMode.Companion.Hue
 import androidx.compose.ui.graphics.BlendMode.Companion.Luminosity
 import androidx.compose.ui.graphics.BlendMode.Companion.Modulate
+import androidx.compose.ui.graphics.BlendMode.Companion.Saturation
 import androidx.compose.ui.graphics.BlendMode.Companion.Softlight
 import androidx.compose.ui.graphics.BlendMode.Companion.Src
 import androidx.compose.ui.graphics.BlendMode.Companion.SrcAtop
@@ -54,6 +60,7 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
@@ -72,7 +79,8 @@ import kotlin.math.roundToInt
 @Composable
 fun BrushPatternRasterImage(modifier: Modifier = Modifier) {
 
-    val polkaDotAsDrawable = ContextCompat.getDrawable( LocalContext.current, R.drawable.baseline_circle_4)
+    val polkaDotAsDrawable =
+        ContextCompat.getDrawable(LocalContext.current, R.drawable.baseline_circle_4)
     val objectToDrawOnAsImageBitmap = ImageBitmap.imageResource(id = R.mipmap.ic_chair_foreground)
     val polkaDotImageBitmap = (polkaDotAsDrawable as VectorDrawable).toBitmap().asImageBitmap()
     val polkaDotBrush = remember(polkaDotImageBitmap) {
@@ -86,27 +94,80 @@ fun BrushPatternRasterImage(modifier: Modifier = Modifier) {
     }
     val polkaDotColor = MaterialTheme.colorScheme.onSurface
 
-    Column (modifier = Modifier.verticalScroll(rememberScrollState(), true),) {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState(), true)) {
         WriteTitleName(titleName = "Draw Raster Image with an all-over dot pattern \nand Red tint, varying BlendMode")
-
-        for (blendModeImage in listOfAllBlendModes()
-            .filter { listOf(Clear, DstIn,DstOut, Dst, Modulate, Softlight, Src, SrcAtop, SrcIn, Xor ).contains(it).not() }) {
-
-            FlowRow(
+        FlowRow(
             modifier = Modifier.background(Color.LightGray),
             verticalArrangement = Arrangement.spacedBy(4.dp),
         ) {
-                for (blendModeColor in listOfAllBlendModes()
-                   .filter { listOf(Clear, Difference, DstOut, Exclusion, Hardlight, Luminosity, Src, SrcAtop, SrcIn, SrcOver, SrcOut, Xor).contains(it).not() }) {
+            for (blendModeImage in listOfAllBlendModes()
+                .filter {
+                    listOf(
+                        Clear,
+                        Dst,
+                        DstIn,
+                         DstOut,
+                         DstOver,
+                         Modulate,
+                         Softlight,
+                        Src,
+                        SrcAtop,
+                         SrcIn,
+                         SrcOver,
+                         Xor
+                    ).contains(it).not()
+                }) {
 
+                for (blendModeColor in listOfAllBlendModes().filter {
+                    val not = listOf(
+                        Clear,
+                        Difference,
+                        Dst, DstAtop,
+                        DstIn,
+                        DstOut,
+                        DstOver,
+                        Exclusion,
+                        Hardlight,
+                        Hue,
+                        Luminosity,
+                        Saturation,
+                        Src,SrcAtop,
+                        SrcIn,
+                        SrcOver,
+                        SrcOut,
+                        Xor
+                    ).contains(it).not()
+                    not
+                }) {
                     Column(
-                    modifier = Modifier
-                        .width(intrinsicSize = IntrinsicSize.Min)
-                        .padding(4.dp)
-                        .border(Dp.Hairline, Color.Black),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                        WriteValue("img:$blendModeImage\ncol:$blendModeColor")
+                        modifier = Modifier
+                            .width(intrinsicSize = IntrinsicSize.Min)
+                            .padding(4.dp)
+                            .border(Dp.Hairline, Color.Black),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(MaterialTheme.colorScheme.secondary)
+                                .padding(start = 4.dp),
+                            text = "img: $blendModeImage",
+                            color = MaterialTheme.colorScheme.onSecondary,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            style = MaterialTheme.typography.labelSmall
+                        )
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(MaterialTheme.colorScheme.secondary)
+                                .padding(start = 4.dp),
+                            text = "col: $blendModeColor",
+                            color = MaterialTheme.colorScheme.onSecondary,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            style = MaterialTheme.typography.labelSmall
+                        )
                         Box(modifier = Modifier
                             .size(90.dp)
                             .graphicsLayer {
@@ -126,20 +187,20 @@ fun BrushPatternRasterImage(modifier: Modifier = Modifier) {
                                         this@drawWithContent.size.height.roundToInt()
                                     ),
                                     colorFilter = ColorFilter.tint(
-                                        Color.Red.copy(alpha = 0.6f),
-                                        blendModeColor
+                                        Color.Red, blendModeColor
                                     ),
                                 )
 
 
-                            }) {
-
-                            // Box content intentionally left blank
-                        }
+                            }) // Box content intentionally left blank
                     }
 
                 }
-                Divider(color = MaterialTheme.colorScheme.primary, thickness = 1.dp, modifier = Modifier.fillMaxWidth())
+                Divider(
+                    color = MaterialTheme.colorScheme.primary,
+                    thickness = 1.dp,
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
         }
     }
@@ -159,38 +220,6 @@ fun BrushRasterImagePreview() {
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun BrushGradientRasterImage(modifier: Modifier = Modifier) {
-
-    val intervals = 12f
-val color1 = Color(0xFFE6E2CE)
-    val color2 = Color(0xFF70F1F2)
-
-    val tilted1Brush = remember {
-        object : ShaderBrush() {
-            override fun createShader(size: Size): Shader {
-                return LinearGradientShader(
-                    colors = listOf(color1, color2),
-                    from = Offset.Zero,
-                    to = Offset(0f, size.height / intervals),
-                    tileMode = TileMode.Mirror
-                )
-            }
-        }
-    }
-    val tilted2Brush = remember {
-        object : ShaderBrush() {
-            override fun createShader(size: Size): Shader {
-                return LinearGradientShader(
-                    colors = listOf(
-                        color1,
-                        color2
-                    ),
-                    from = Offset(size.width / intervals, 0f),
-                    to = Offset(0f, 0f),
-                    tileMode = TileMode.Mirror
-                )
-            }
-        }
-    }
 
     val objectToFillImage = ImageBitmap.imageResource(id = R.mipmap.ic_chair_foreground)
 
@@ -217,27 +246,56 @@ val color1 = Color(0xFFE6E2CE)
                         .padding(4.dp)
                         .size(50.dp)
                         .graphicsLayer(compositingStrategy = CompositingStrategy.Offscreen)
-                        .drawWithContent {
-                            drawRect(
-                                brush = tilted1Brush,
-                                alpha = 1f,
-                            )
-                            drawRect(
-                                brush = tilted2Brush,
-                                alpha = .5f,
-                            )
-                            drawImage(
-                                image = objectToFillImage,
-                                blendMode = blendMode,
-                                dstSize = IntSize(
-                                    this@drawWithContent.size.width.roundToInt(),
-                                    this@drawWithContent.size.height.roundToInt()
-                                ),
-                            )
+                        .drawWithCache {
 
-                        }) {
-                        // Box content intentionally left blank
-                    }
+                            val intervals = 12f
+                            val color1 = Color(0xFFE6E2CE)
+                            val color2 = Color(0xFF70F1F2)
+
+                            val tilted1Brush = object : ShaderBrush() {
+                                override fun createShader(size: Size): Shader {
+                                    return LinearGradientShader(
+                                        colors = listOf(color1, color2),
+                                        from = Offset.Zero,
+                                        to = Offset(0f, size.height / intervals),
+                                        tileMode = TileMode.Mirror
+                                    )
+                                }
+                            }
+
+                            val tilted2Brush = object : ShaderBrush() {
+                                override fun createShader(size: Size): Shader {
+                                    return LinearGradientShader(
+                                        colors = listOf(
+                                            color1, color2
+                                        ),
+                                        from = Offset(size.width / intervals, 0f),
+                                        to = Offset(0f, 0f),
+                                        tileMode = TileMode.Mirror
+                                    )
+                                }
+                            }
+
+                            onDrawWithContent {
+                                drawRect(
+                                    brush = tilted1Brush,
+                                    alpha = 1f,
+                                )
+                                drawRect(
+                                    brush = tilted2Brush,
+                                    alpha = .5f,
+                                )
+                                drawImage(
+                                    image = objectToFillImage,
+                                    blendMode = blendMode,
+                                    dstSize = IntSize(
+                                        this@onDrawWithContent.size.width.roundToInt(),
+                                        this@onDrawWithContent.size.height.roundToInt()
+                                    ),
+                                )
+
+                            }
+                        }) // Box content intentionally left blank
                 }
             }
         }
